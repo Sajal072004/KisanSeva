99e5420a8219a0ac7cd2f3a04cd209c99627a1fe
PrathamDwivedi27 <pratham.gungun12345@gmail.com>
2024-09-06 02:57:38 +0530

    revert the previous changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,6 @@ import { setupJobs } from '../utils/job.js'
 //Middlewares
 const app=express();
 app.use(cors());
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
